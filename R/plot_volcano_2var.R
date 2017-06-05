e8632cb92e2bd116c5ac2ce7ec0a0b4ff906faeb
Matt Dufort <matt.dufort@gmail.com>
2017-06-05 09:53:48 -0700

    Add flexibility to gene labeling in plot_volcano_2var

--- R/plot_volcano_2var.R
+++ R/plot_volcano_2var.R
@@ -12,10 +12,14 @@
 #' @param fc_cut numeric, the (absolute value) log2 fold-change threshold for determining significance of genes. This value is also plotted as vertical dotted lines. Setting to NULL removes the lines.
 #' @param p_cut numeric, the p-value threshold for determining significance of genes. This value is also plotted as a horizontal dotted line. Setting to NULL removes the lines.
 #' @param x_lim,y_lim either "auto", NULL, or numeric vectors. If "auto", x- and y-limits are determined from the data using \code{get_xy_lims}. If NULL, default plot limits are used. If provided as numeric vectors, the lower and upper limits of the plotting space along the x- and y-axes. Passed to \code{ggplot2::xlim}.
-#' @param gene_labs logical, whether to include gene labels for genes with extreme logFC and p-value. If \code{TRUE}, genes with values outside the labeling ellipse will be labeled.
+#' @param gene_labs character, the type of gene labeling to include. If \code{"threshold"}. If \code{"ellipse"}, genes with values outside the labeling ellipse will be labeled. Default value is NULL, which yields no gene labeling.
 #' @param x_cut,y_cut numeric, the radii of the labeling ellipse along the x- and y-axes. Genes with values outside the ellipse are labeled with gene names. Default to 0, which results in all genes being labeled.
+#' @param x_cut_direction character. For threshold-based gene labeling only, specifies the direction of the threshold for gene labeling. Default to "both", which labels genes with absolute value logFC greater than x_cut. "lower" labels genes with logFC less than x_cut; "upper" labels genes with logFC greater than x_cut.
+#' @param gene_labs_repel logical, whether to force separation of the gene label text. If TRUE, labels are plotted using \code{geom_text_repel}.
+#' @param gene_lab_size numeric, the size of the gene label text. Passed to \code{geom_text} to \code{geom_text_repel}.
 #' @param ... additional parameters passed to \code{pdf}.
 #' @import ggplot2
+#' @import ggrepel
 #' @export
 #' @usage \code{
 #' plot_volcano_2var(
@@ -23,14 +27,16 @@
 #'      file_prefix=NULL, plotdims=c(9,9),
 #'      color_by_threshold=TRUE, fc_cut=log2(1.5), p_cut=0.01,
 #'      x_lim="auto", y_lim="auto",
-#'      gene_labs=FALSE, x_cut=0, y_cut=0,
+#'      gene_labs=NULL, x_cut=0, y_cut=0, x_cut_direction="both",
+#'      gene_labs_repel=TRUE, gene_lab_size=3,
 #'      ...)}
 plot_volcano_2var <-
   function(topGenes, my_cols=c("darkcyan", "darkorange"),
            file_prefix=NULL, plotdims=c(9,9),
            color_by_threshold=TRUE, fc_cut=log2(1.5), p_cut=0.01,
            x_lim="auto", y_lim="auto",
-           gene_labs=FALSE, x_cut=0, y_cut=0,
+           gene_labs=NULL, x_cut=0, y_cut=0, x_cut_direction="both",
+           gene_labs_repel=TRUE, gene_lab_size=3,
            ...) {
     if (color_by_threshold & (is.null(fc_cut) | is.null(p_cut)))
       stop("Cannot plot points by threshold with null values of fc_cut or p_cut.")
@@ -77,12 +83,43 @@ plot_volcano_2var <-
     
     if (!is.null(x_lim)) {volcano <- volcano + xlim(x_lim)}
     if (!is.null(y_lim)) {volcano <- volcano + ylim(y_lim)}
-    if (gene_labs) {
-      volcano <- volcano +
-        geom_text(
-          data=topGenes[((topGenes$logFC^2)/(x_cut^2) + (log10(topGenes$adj.P.Val)^2)/(y_cut^2)) > 1,],
-          aes(label=genes),
-          color="black", size=3, vjust=1, hjust=0.5)}
+    
+    if (!is.null(gene_labs)) {
+      gene_labs <- match.arg(gene_labs, choices=c("ellipse", "threshold"))
+      if (gene_labs=="ellipse") {
+        topGenes.tmp <-
+          topGenes[((topGenes$logFC^2)/(x_cut^2) + (log10(topGenes$adj.P.Val)^2)/(y_cut^2)) > 1,]
+      } else if (gene_labs=="threshold") {
+        x_cut_direction <- match.arg(x_cut_direction, choices=c("both", "lower", "upper"))
+        if (x_cut_direction=="both") {
+          topGenes.tmp <-
+            topGenes[
+              (abs(topGenes$logFC) > x_cut) & (-log10(topGenes$adj.P.Val) > y_cut),]
+        } else if (x_cut_direction=="lower") {
+          topGenes.tmp <-
+            topGenes[
+              (topGenes$logFC < x_cut) & (-log10(topGenes$adj.P.Val) > y_cut),]
+        } else if (x_cut_direction=="upper") {
+          topGenes.tmp <-
+            topGenes[
+              (topGenes$logFC > x_cut) & (-log10(topGenes$adj.P.Val) > y_cut),]
+        }
+      }
+      
+      if (gene_labs_repel) {
+        volcano <- volcano +
+          geom_text_repel(
+            data=topGenes.tmp,
+            aes(label=genes),
+            color="black", size=gene_lab_size)
+      } else {
+        volcano <- volcano +
+          geom_text(
+            data=topGenes.tmp,
+            aes(label=genes),
+            color="black", size=gene_lab_size, vjust=1, hjust=0.5)
+      }
+    }
     
     # output volcano plot to file or plot window
     if (!is.null(file_prefix)) {
